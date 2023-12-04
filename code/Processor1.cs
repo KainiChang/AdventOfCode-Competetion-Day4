f1f72f47c43a3bffa25b55b757d54afd497677a5
a1866621 <a1866621@adelaide.edu.au>
2023-12-04 17:05:09 +1030

    feat: passed both

--- code/Processor1.cs
+++ code/Processor1.cs
@@ -1,61 +1,36 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text.RegularExpressions;
 
 namespace code;
 
 public class Processor1
 {
-    public static long Process(string[] input, int red, int blue, int green)
+    public static long Process(string[] input)
     {
         long sum = 0;
         for (int i = 0; i < input.Length; i++)
         {
-            string gameData = input[i].Substring(input[i].IndexOf(':') + 1).Trim(); // Extracting the game data part
-
-            if (BiggerThanLargestCount(gameData, red,blue,green) )
+            string gameData = input[i].Substring(input[i].IndexOf(':') + 1).Trim(); 
+            var parts = gameData.Split('|');
+            var firstPart = Regex.Split(parts[0].Trim(), @"\s+");
+            var secondPart = Regex.Split(parts[1].Trim(), @"\s+");
+            int count = 0;
+            // if any element in secondPart is contained in the first part, count ++
+            foreach (var item in secondPart)
             {
-                sum = sum + 1 + i;
-            }                                 
+                if (firstPart.Contains(item))
+                {
+                    count++;
+                }
+            }
+            if (count > 0)
+            {
+                sum = sum + (long)Math.Pow(2, count - 1);
+            }
         }
 
         return sum;
 
     }
 
-    public static bool BiggerThanLargestCount(string gameData, int red, int blue, int green)
-    {
-        var parts = gameData.Split(new char[] {';',','},StringSplitOptions.RemoveEmptyEntries);
-
-                foreach (var part in parts)
-                {
-                    var elements = part.Trim().Split(' ');
-                    if (elements.Length == 2 && int.TryParse(elements[0], out int count))
-                    {
-                        switch (elements[1].ToLower())
-                        {
-                            case "red":
-                                if (count > red)
-                                {
-                                    return false;
-                                }
-                                break;
-                            case "blue":
-                                if (count > blue)
-                                {
-                                    return false;
-                                }
-                                break;
-                            case "green":
-                                if (count > green)
-                                {
-                                    return false;
-                                }
-                                break;
-                        }
-                    }
-                }
-        return true;
-    }
+  
 }
--- code/Processor2.cs
+++ code/Processor2.cs
@@ -1,6 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text.RegularExpressions;
 
 namespace code;
@@ -9,45 +6,59 @@ public class Processor2
 {
     public static long Process(string[] input)
     {
-        long power = 0;
+        // define an array to record the count of matches
+        int[] cardArray = new int[input.Length];
+
         for (int i = 0; i < input.Length; i++)
         {
-            string gameData = input[i].Substring(input[i].IndexOf(':') + 1).Trim(); 
-            power = power + GetPowerOfMaxCount(gameData);
+            string gameData = input[i].Substring(input[i].IndexOf(':') + 1).Trim(); // Extracting the game data part
+                                                                                    // Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+                                                                                    // separate the game data into two int list by '|'
+            var parts = gameData.Split('|');
+            var firstPart = Regex.Split(parts[0].Trim(), @"\s+");
+            var secondPart = Regex.Split(parts[1].Trim(), @"\s+");
+            int count = 0;
+            // if any element in secondPart is contained in the first part, count ++
+            foreach (var item in secondPart)
+            {
+                if (firstPart.Contains(item))
+                {
+                    count++;
+                }
+            }
 
+            cardArray[i] = count;
+        }
+        long numberOfCards = CalculateTheNumberOfCards(cardArray);
+        return numberOfCards;
+
+    }
+
+    public static long CalculateTheNumberOfCards(int[] cardArray)
+    {
+        //create an array to record each number of card
+        int[] numberOfCards = new int[cardArray.Length];
+        // set each element to 1
+        for (int i = 0; i < numberOfCards.Length; i++)
+        {
+            numberOfCards[i] = 1;
         }
 
-        return power;
-
+        // count of matches of i affect the following cards
+        for (int i = 0; i < cardArray.Length; i++)
+        {
+            for (int j = i + 1; j <= cardArray[i] + i; j++)
+            {
+                // number of i affect the addition of the following card's number
+                numberOfCards[j] = numberOfCards[j] + numberOfCards[i];
+            }
+        }
+        long sum = 0;
+        // sum elements in numberOfCards
+        for (int i = 0; i < numberOfCards.Length; i++)
+        {
+            sum = sum + numberOfCards[i];
+        }
+        return sum;
     }
-    public static long GetPowerOfMaxCount(string gameData)
-    {
-        var segments = gameData.Split(';');
-        int maxRed = 0, maxBlue = 0, maxGreen = 0;
-
-
-        var parts = gameData.Split(new char[] {';',','},StringSplitOptions.RemoveEmptyEntries);
-
-                foreach (var part in parts)
-                {
-                    var elements = part.Trim().Split(' ');
-                    if (elements.Length == 2 && int.TryParse(elements[0], out int count))
-                    {
-                        switch (elements[1].ToLower())
-                        {
-                            case "red":
-                                maxRed = Math.Max(maxRed, count);
-                                break;
-                            case "blue":
-                                maxBlue = Math.Max(maxBlue, count);
-                                break;
-                            case "green":
-                                maxGreen = Math.Max(maxGreen, count);
-                                break;
-                        }
-                    }
-                }
-        return maxRed * maxBlue * maxGreen;
-    }
-
 }
